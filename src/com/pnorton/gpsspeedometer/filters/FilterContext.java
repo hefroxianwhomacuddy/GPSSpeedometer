0bfbee19586aa4a13a754d2ff6ed0b417803fe57
Peter <Peter@GIGAAMD64>
2013-01-14 13:28:40 +0000

    Comments for FilterContext class

--- src/com/pnorton/gpsspeedometer/filters/FilterContext.java
+++ src/com/pnorton/gpsspeedometer/filters/FilterContext.java
@@ -1,24 +1,45 @@
 package com.pnorton.gpsspeedometer.filters;
 
+/**
+ * FilterContext Strategy Class
+ * @author Peter B Norton
+ *
+ */
 public class FilterContext {
 	
 	private Filter m_filter;
 	
+	/**
+	 * Construct a FilterContext with a specified Filter
+	 * @param filter Filter to set
+	 */
 	public FilterContext(Filter filter)
 	{
 		m_filter = filter;
 	}
 	
+	/**
+	 * Reset the Filter
+	 */
 	public void reset()
 	{
 		m_filter.reset();
 	}
 	
+	/**
+	 * Reset the filter and specify weights
+	 * @param weights Number of weights (May not be used by all filters
+	 */
 	public void reset(int weights)
 	{
 		m_filter.reset(weights);
 	}
 	
+	/**
+	 * Process a sample through the filter
+	 * @param sample Sample to process
+	 * @return processed result
+	 */
 	public float processSample(float sample)
 	{
 		return m_filter.processSample(sample);
